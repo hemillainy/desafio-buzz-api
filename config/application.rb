e1a96633c4f858af861784a148c337c5e1adac7b
hemillainysantos <hemillainy.santos@ccc.ufcg.edu.br>
2018-10-18 03:05:53 -0300

    att

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,6 @@ module Api
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.assets.initialize_on_precompile = false
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
